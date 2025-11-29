5592a0d55ecb8cf73a85f1e311e4a6ba9ae67497
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-29 10:54:50 -0600

    module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   nitro: {
-    preset: 'cloudflare',
+    preset: 'cloudflare-module',
     modules: [additionalModules({ target: "edge-light" })],
     rollupConfig: {
       plugins: [vue()]
